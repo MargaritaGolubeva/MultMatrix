aa93683b0264cc35a7dc3265ef9a5d4076b5b9d6
MargaritaGolubeva <golubeva.rita2013@yandex.ru>
2019-02-24 19:11:54 +0700

    Add files via upload

---
+++ Mult matrix Parallel/Mult matrix Parallel/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mult_matrix_Parallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        double[,] MultMatrix(int size, double[,] matrix1, double[,] matrix2)
+        {
+            var result = new double[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < size; k++)
+                    {
+                        result[i, j] += matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+
+        double[,] MultMatrixParallel(int size, double[,] matrix1, double[,] matrix2)
+        {
+            var result = new double[size, size];
+            Parallel.For(0, size,
+              i =>
+              {
+                  for (int j = 0; j < size; j++)
+                  {
+                      result[i, j] = 0;
+                      for (int k = 0; k < size; k++)
+                      {
+                          result[i, j] += matrix1[i, k] * matrix2[k, j];
+                      }
+                  }
+              });
+            return result;
+        }
+    }
+}
